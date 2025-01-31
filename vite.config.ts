4033b1300206b19d3ebd29414c32cd7e09ab595b
Danil Kondratov <com.kondratov@gmail.com>
2025-01-31 17:03:01 +0300

    test

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-    base: './',
+    base: '/KolhoznikVsTrump/',
     build: {
         outDir: 'dist',
         rollupOptions: {
